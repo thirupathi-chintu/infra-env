7f89c875d35c4e9ff0f67a5838f6eb201181cf10
thirupathi-p <thirupathi8p@yahoo.com>
2019-08-19 13:39:06 +0530

    Finally gitignore

--- genericenv/main.tf
+++ genericenv/main.tf
@@ -4,25 +4,30 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 }
+# if [ ENV = "dev" ]
+# then
+#   module "dev" {
+#     source = "./dev"
+#     ENV = "dev"
+#   }
+#  fi
 
-if  ( ENV == "dev") {
+
     module "dev" {
     source = "./dev"
     ENV = "dev"
   }
-}
 
-elsif  ( ENV == "jenkins") {
+
     module "jenkins" {
     source = "./jenkins"
     ENV = "jenkins"
   }
-}
 
-elsif  ( ENV == "prod") {
+
     module "prod" {
     source = "./prod"
     ENV = "prod"
   }
-}
+
 
